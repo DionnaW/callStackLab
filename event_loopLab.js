4bf0500f28cae7654c9e4a224061d12c0f87f8de
Dionna <businessladythick@yahoo.com>
2024-03-06 12:27:23 -0500

    adding counter var just shows increase by 1/error

--- event_loopLab.js
+++ event_loopLab.js
@@ -2,10 +2,12 @@
 var counter = 0;
 
 //function that increments the variable & calls itself recursively
+//adding counter var in catch to see output
 function incrementAndCall() {
     try {
         counter++;
         console.log(counter); 
+        console.log('Counter value:', counter);
     
 
     //calling function recursively % ran as not defined
@@ -21,6 +23,7 @@ try {
     incrementAndCall();
  } catch (error) {
     console.error('An error occurred:', error);
+    console.log('Counter value:', counter)
  }
 
 //i ran code and got output of 1 An error coourred
