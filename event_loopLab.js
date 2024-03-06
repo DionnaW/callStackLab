2d5a8182ca6e482ad2a2aa160d84fa85ca70236b
Dionna <businessladythick@yahoo.com>
2024-03-06 12:41:16 -0500

    max count made loop stp at 15 no error

--- event_loopLab.js
+++ event_loopLab.js
@@ -1,20 +1,28 @@
 //declared var
 var counter = 0;
-
+var maxCount = 15;
+// 
 //function that increments the variable & calls itself recursively
 //adding counter var in catch to see output
 function incrementAndCall() {
     try {
         counter++;
         console.log(counter); 
-        console.log('Counter value:', counter);
-    
+        // console.log('Counter value:', counter);
 
-    //calling function recursively % ran as not defined
+    //checking if counter reached max count
+    if (counter === maxCount) {
+        console.log('Maximun count reached.');
+        return; //exit if max count is reached
+    }
+    // 
+
+    //calling function recursively  ran as not defined
     //adding try/catch to check for error output
-    incrementAndall();
+    incrementAndCall();
     }  catch (error) {
-        console.error('An error occurred:');
+        console.error('An error occurred:', error);
+        console.log('Counter value:', counter)
     }
 }
 
@@ -26,6 +34,6 @@ try {
     console.log('Counter value:', counter)
  }
 
-//i ran code and got output of 1 An error coourred
-//so I'm adding a max count as an exit condition to see what uotput i get
+//i ran code and got output of 1 An error coourred each time
+//I'm adding a max count as an exit condition to see what output i get
 
